f2edf8205dffd05b944affc1d0ad010c60dcb4da
Daniel Romero <leogolplex@gmail.com>
2022-06-28 14:50:50 -0500

    Add queue example

---
+++ WorkQueues/NewTask/NewTask.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "user",
+    Password = "password",
+    Port = 5672,
+};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "task_queue",
+                         durable: true,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var message = GetMessage(args);
+    var body = Encoding.UTF8.GetBytes(message);
+
+    var properties = channel.CreateBasicProperties();
+    properties.Persistent = true;
+
+    channel.BasicPublish(exchange: "",
+                         routingKey: "task_queue",
+                         basicProperties: properties,
+                         body: body);
+    Console.WriteLine(" [x] Sent {0}", message);
+}
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+}
